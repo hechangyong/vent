e941445ae23ffa109ad7eae81d1cf26b82cbce99
hecy <hechangyong@aliyun.com>
2018-07-17 14:23:55 +0800

    start

---
+++ src/main/java/com/im/vent/VentApplication.java
@@ -0,0 +1,12 @@
+package com.im.vent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VentApplication.class, args);
+	}
+}
