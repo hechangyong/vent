d34f5df9e2197cbd1dc1276efeff646201c1626c
hechangyong <408389763@qq.com>
2018-07-18 22:49:00 +0800

    优化

---
+++ src/main/java/com/im/vent/baiduapi/UnitService.java
@@ -0,0 +1,57 @@
+package com.im.vent.baiduapi;
+
+/**
+ * Created by hecy on 2018/7/18.
+ */
+/*
+ * unit对话服务
+ */
+public class UnitService {
+    /**
+     * 重要提示代码中所需工具类
+     * FileUtil,Base64Util,HttpUtil,GsonUtils请从
+     * https://ai.baidu.com/file/658A35ABAB2D404FBF903F64D47C1F72
+     * https://ai.baidu.com/file/C8D81F3301E24D2892968F09AE1AD6E2
+     * https://ai.baidu.com/file/544D677F5D4E4F17B4122FBD60DB82B3
+     * https://ai.baidu.com/file/470B3ACCA3FE43788B5A963BF0B625F3
+     * 下载
+     */
+    private static String utterance(String msg, String user_id, String token) {
+        // 请求URL
+        String talkUrl = "https://aip.baidubce.com/rpc/2.0/unit/bot/chat";
+        try {
+            // 请求参数
+            String params = "{" +
+                    "\"bot_session\":\"\"," +
+                    "\"log_id\":\"7758521\"," +
+                    "\"request\":{" +
+                    "   \"bernard_level\":1," +
+                    "   \"client_session\":\"{" +
+                    "       \\\"client_results\\\":\\\"\\\", " +
+                    "       \\\"candidate_options\\\":[]" +
+                    "   }\"," +
+                    "   \"query\":\""+msg+"\"," +
+                    "   \"query_info\":{" +
+                    "       \"source\":\"KEYBOARD\"," +
+                    "       \"type\":\"TEXT\"" +
+                    "   }," +
+                    "   \"updates\":\"\"," +
+                    "   \"user_id\":\""+user_id+"\"" +
+                    "}," +
+                    "\"bot_id\":7835," +
+                    "\"version\":\"2.0\"" +
+                    "}";
+//            String accessToken =  "24.04caed631a99a706b68d747e1cb38370.2592000.1534493845.282335-11547509";
+            String result = HttpUtil.post(talkUrl, token, "application/json", params);
+            return result;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+    public static void main(String[] args) {
+
+
+           utterance("烦","001","");
+    }
+}
---
+++ src/main/java/com/im/vent/utils/DateUtils.java
@@ -0,0 +1,16 @@
+package com.im.vent.utils;
+
+import java.text.SimpleDateFormat;
+
+/**
+ * Created by hecy on 2018/7/17.
+ */
+public class DateUtils {
+
+    public static  String yyyyMMdd = "yyyy-MM-dd";
+    public static  String yyyyMMddHHmmss = "yyyy-MM-dd HH:mm:ss";
+    public static  SimpleDateFormat yyyyMMddHHmmssSDF = new SimpleDateFormat(yyyyMMddHHmmss);
+    public static  SimpleDateFormat yyyyMMddSDF = new SimpleDateFormat(yyyyMMdd);
+
+
+}
